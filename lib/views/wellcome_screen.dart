f28cf43c26eec4115c42ae5cc1754acb688bf471
MdAsifUrRahmanAbir <mdasifurrahmanabir@gmail.com>
2022-05-17 01:03:02 +0600

    doing splash and welcome

--- lib/views/wellcome_screen.dart
+++ lib/views/wellcome_screen.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+import 'package:chondo_flutter_project/models/all_views.dart';
+
 
 class WelcomeScreen extends StatelessWidget {
    WelcomeScreen({Key? key}) : super(key: key);
@@ -14,9 +18,9 @@ class WelcomeScreen extends StatelessWidget {
     ];
 
     List welcomeText = [
-      'Text1',
-      'Text2',
-      'Text3'
+      'Track Your Period',
+      'Share Report With Your\nPartner & Doctor',
+      'Know Your Body\nBetter Than Ever'
     ];
 
     List welcomeBar = [
@@ -40,28 +44,72 @@ class WelcomeScreen extends StatelessWidget {
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
+                  Spacer(),
                   Container(
-                      padding: EdgeInsets.only(top: 145),
                       margin: EdgeInsets.symmetric( horizontal: 50),
-                      child: Image.asset('assets/logos/welcomelogo1.png',)),
-                  Text('Data'),
-                  const SizedBox(height: 20,),
+                      child: Image.asset(welcomeImage[index],)),
 
-                  Container(
-                    height: 8,
-                    width: 20,
-                    child: Image.asset(welcomeBar[index]),
+                  SizedBox(height: 70,),
+
+                  Column(
+                    children: [
+                      Text(welcomeText[index], style: GoogleFonts.roboto( color: Colors.black, fontSize: 28),),
+                      const SizedBox(height: 18,),
+                      SizedBox(
+                        height: 8,
+                        width: 20,
+                        child: Image.asset(welcomeBar[index], fit: BoxFit.fitWidth),
+                      ),
+                    ],
                   ),
 
-                  Container(
-                    height: 20,
-                    width: 50,
-                    color: Colors.red,
-                  )
+                  Spacer(),
+
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Container(),
+                      index == 2? _startNow(context): _next(context),
+                    ],
+                  ),
+                  SizedBox(height: 30,)
+
                 ],
               ),
             );
           }
+      ),
+    );
+  }
+
+  _next(BuildContext context){
+    return Container(
+      margin: EdgeInsets.only(left: 210, ),
+      alignment: Alignment.bottomRight,
+      height: 60,
+      width: 60,
+      decoration: const BoxDecoration(
+        image: DecorationImage(image: AssetImage('assets/buttons/welcomebutton.png'))
+      ),
+    );
+  }
+
+  _startNow(BuildContext context){
+    return InkWell(
+
+      onTap: (){
+        Navigator.pushAndRemoveUntil(context, MaterialPageRoute(builder: (context)=> HomeViews()), (route) => false);
+      },
+
+      child: Container(
+        alignment: Alignment.center,
+        height: 59,
+        width: 265,
+        decoration: const BoxDecoration(
+            image: DecorationImage(image: AssetImage('assets/buttons/startnow.png'))
+        ),
+        child: const Text('Start Now', style: TextStyle(color: Colors.white, fontWeight: FontWeight.w700),),
       ),
     );
   }
